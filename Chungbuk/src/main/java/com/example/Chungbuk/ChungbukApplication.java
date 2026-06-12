3060d04e1b384d9b735f40dd20892a27fa610d53
junggayeong <jky303101@gmail.com>
2026-06-12 22:13:34 +0900

    init: 프로젝트 기본 구조 세팅

---
+++ Chungbuk/src/main/java/com/example/Chungbuk/ChungbukApplication.java
@@ -0,0 +1,13 @@
+package com.example.Chungbuk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChungbukApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChungbukApplication.class, args);
+	}
+
+}
---
+++ Chungbuk/src/test/java/com/example/Chungbuk/ChungbukApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Chungbuk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChungbukApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
