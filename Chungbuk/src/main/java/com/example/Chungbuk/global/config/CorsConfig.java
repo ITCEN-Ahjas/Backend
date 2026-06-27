ee30ae3356860159ebf2020c3fae69fc27c059f4
junggayeong <152073691+junggayeong@users.noreply.github.com>
2026-06-27 20:43:44 +0900

    Feat: 배포준비

--- Chungbuk/src/main/java/com/example/Chungbuk/global/config/CorsConfig.java
+++ Chungbuk/src/main/java/com/example/Chungbuk/global/config/CorsConfig.java
@@ -19,7 +19,8 @@ public class CorsConfig {
                                 "http://localhost:3000",
                                 "http://localhost:5173",
                                 "http://127.0.0.1:3000",
-                                "http://127.0.0.1:5173"
+                                "http://127.0.0.1:5173",
+                                "https://여기에_Amplify주소.amplifyapp.com"
                         )
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
